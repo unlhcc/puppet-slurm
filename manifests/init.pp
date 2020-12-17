b3a8938dee359d06010da44ccb8486309ba041f0
Josh Samuelson <jsamuels@cse.unl.edu>
2020-12-17 09:34:43 -0600

    Updates for RHEL8
    - puppet-lint fixups here and there
    - Enum role variable, default to 'other' when not specified
    - bring job_submit.lua under puppet control for slurmctld
    - bring slurmdbd.conf under puppet control for slurmdbd
    - ensure various directories/files are present before starting services
    - switch to https://github.com/stanford-rc/slurm-spank-lua
    - guard definition of lua-posix package in case other class defines it
    - add force to /etc/slurm/lua.d resource since some packages create
      subdirectories under that path (i.e., slurm-spank-lua)

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,15 +3,20 @@
 #
 
 class slurm (
-    $user                  = $slurm::params::user,
-    $role                  = $slurm::params::role,
-    $config                = $slurm::params::config,
-    $cgroup                = $slurm::params::cgroup,
-    $gres                  = $slurm::params::gres,
-    $plugstack             = $slurm::params::plugstack,
-    $lua                   = $slurm::params::lua,
-    $topology              = $slurm::params::topology,
-    $slurmd_service_dropin = $slurm::params::slurmd_service_dropin,
+    String $user                                 = $slurm::params::user,
+    Enum['compute', 'controller', 'other'] $role = 'other',
+    String $config                               = $slurm::params::config,
+    String $cgroup                               = $slurm::params::cgroup,
+    String $gres                                 = $slurm::params::gres,
+    String $plugstack                            = $slurm::params::plugstack,
+    String $job_submit_lua                       = $slurm::params::job_submit_lua,
+    String $lua                                  = $slurm::params::lua,
+    String $topology                             = $slurm::params::topology,
+    String $slurmdbd_storagehost                 = $slurm::params::slurmdbd_storagehost,
+    String $slurmdbd_storageuser                 = $slurm::params::slurmdbd_storageuser,
+    String $slurmdbd_storagepass                 = $slurm::params::slurmdbd_storagepass,
+    String $slurmdbd_storageloc                  = $slurm::params::slurmdbd_storageloc,
+    String $slurmd_service_dropin                = $slurm::params::slurmd_service_dropin,
   ) inherits slurm::params {
 
     ########################################################
@@ -32,7 +37,7 @@ class slurm (
     }
 
     file { '/etc/slurm/cgroup.conf':
-        source  => $cgroup,
+        source => $cgroup,
     }
 
     file { '/etc/slurm/topology.conf':
@@ -43,16 +48,16 @@ class slurm (
         source => $topology,
     }
 
-    user { "slurm":
-        name       => $user,
+    user { 'slurm':
         ensure     => present,
+        name       => $user,
         system     => true,
         managehome => false,
         shell      => '/sbin/nologin',
         # Debian reserved uid/gid
         # http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=444412
         uid        => 64030,
-        gid        => "nobody",
+        gid        => 'nobody',
     }
 
     package { 'slurm':
@@ -70,19 +75,20 @@ class slurm (
     if $role == 'compute' {
 
         file { '/etc/slurm/plugstack.conf':
-            source  => $plugstack,
+            source => $plugstack,
         }
 
         file { '/etc/slurm/lua.d':
             source  => $lua,
             recurse => true,
             purge   => true,
+            force   => true,
         }
 
         if $gres {
             file { '/etc/slurm/gres.conf':
-                source  => $gres,
-                notify  => Service['slurmd'],
+                source => $gres,
+                notify => Service['slurmd'],
             }
         }
 
@@ -91,17 +97,21 @@ class slurm (
         }
 
         # Packages needed for SLURM Lua plugins
-        package { 'lua-posix':
-            ensure => present,
+        if ! defined(Package['lua-posix']) {
+            package { 'lua-posix':
+                ensure => present,
+            }
         }
         package { 'lua-linuxsys':
             ensure => present,
         }
-        package { 'slurm-spank-plugins-lua':
+        package { 'slurm-spank-lua':
             ensure  => present,
             # This package runs the Lua plugins
-            require => [ Package['lua-posix', 'lua-linuxsys'],
-                         File['/etc/slurm/lua.d'], ],
+            require => [
+                Package['lua-posix', 'lua-linuxsys'],
+                File['/etc/slurm/lua.d'],
+            ],
         }
 
         # Running on compute nodes/workers
@@ -109,9 +119,11 @@ class slurm (
             ensure    => running,
             enable    => true,
             require   => Package['slurm-slurmd'],
-            subscribe => [ File['/etc/slurm/slurm.conf'],
-                           File['/etc/slurm/cgroup.conf'],
-                           File['/etc/slurm/plugstack.conf'], ],
+            subscribe => [
+                File['/etc/slurm/slurm.conf'],
+                File['/etc/slurm/cgroup.conf'],
+                File['/etc/slurm/plugstack.conf'],
+            ],
         }
 
         file { '/etc/systemd/system/slurmd.service.d':
@@ -126,7 +138,7 @@ class slurm (
             owner  => 'root',
             group  => 'root',
             mode   => '0644',
-            source  => $slurmd_service_dropin,
+            source => $slurmd_service_dropin,
         }
 
         exec { '/usr/bin/systemctl daemon-reload':
@@ -145,23 +157,89 @@ class slurm (
             ensure => present,
         }
 
-        # Running on controller
+        # dir needed by slurmdbd and slurmctld
+        file { '/var/log/slurm':
+            ensure => 'directory',
+            owner  => 'slurm',
+            group  => 'nobody',
+            mode   => '0755',
+        }
+
+        # files/dirs needed by slurmdbd
+        $slurmdbd_parameters = {
+            storagehost => $slurmdbd_storagehost,
+            storageuser => $slurmdbd_storageuser,
+            storagepass => $slurmdbd_storagepass,
+            storageloc  => $slurmdbd_storageloc,
+        }
+
+        file { '/etc/slurm/slurmdbd.conf':
+            ensure  => 'file',
+            owner   => 'slurm',
+            group   => 'nobody',
+            mode    => '0600',
+            content => epp('common/slurm/slurmdbd.conf.epp',
+                            $slurmdbd_parameters),
+        }
+
+        file { '/var/log/slurm/archive':
+            ensure => 'directory',
+            owner  => 'slurm',
+            group  => 'nobody',
+            mode   => '0755',
+        }
+
+        file { '/var/log/slurm/slurmdbd.log':
+            ensure => 'file',
+            owner  => 'slurm',
+            group  => 'nobody',
+            mode   => '0600',
+        }
+
+        # Running on controller host
         service { 'slurmdbd':
-            ensure    => running,
-            enable    => true,
-            require   => Package['slurm-slurmdbd'],
+            ensure  => running,
+            enable  => true,
+            require => Package['slurm-slurmdbd'],
+        }
+
+        # files/dirs needed by slurmctld
+        file { '/etc/slurm/job_submit.lua':
+            ensure => 'file',
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0644',
+            source => $job_submit_lua,
+        }
+
+        file { '/var/log/slurm/slurmctld.log':
+            ensure => 'file',
+            owner  => 'slurm',
+            group  => 'nobody',
+            mode   => '0600',
+        }
+
+        file { '/var/spool/slurmd':
+            ensure => 'directory',
+            owner  => 'slurm',
+            group  => 'nobody',
+            mode   => '0700',
         }
 
         service { 'slurmctld':
             ensure    => running,
             enable    => true,
-            require   => [ Service['slurmdbd'],
-                           Package['slurm-slurmctld'], ],
-            subscribe => [ File['/etc/slurm/slurm.conf'],
-                           File['/etc/slurm/cgroup.conf'],
-                           File['/etc/slurm/topology.conf'], ],
+            require   => [
+                Service['slurmdbd'],
+                Package['slurm-slurmctld'],
+            ],
+            subscribe => [
+                File['/etc/slurm/slurm.conf'],
+                File['/etc/slurm/cgroup.conf'],
+                File['/etc/slurm/topology.conf'],
+            ],
         }
-    } else {
+    } elsif $role == 'other' {
         # Installed and configured, but not running on other roles
     }
 
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,13 +1,21 @@
+#
+# Class: slurm::params
+#
+
 class slurm::params {
 
     $user                  = 'slurm'
-    $role                  = 'compute'
     $config                = 'puppet:///modules/common/slurm/slurm.conf'
     $cgroup                = 'puppet:///modules/common/slurm/cgroup.conf'
     $gres                  = 'puppet:///modules/common/slurm/gres.conf'
     $plugstack             = 'puppet:///modules/common/slurm/plugstack.conf'
+    $job_submit_lua        = 'puppet:///modules/common/slurm/job_submit.lua'
     $lua                   = 'puppet:///modules/common/slurm/lua.d'
     $topology              = 'puppet:///modules/common/slurm/topology.conf'
+    $slurmdbd_storagehost  = 'localhost'
+    $slurmdbd_storageuser  = 'slurm'
+    $slurmdbd_storagepass  = ''
+    $slurmdbd_storageloc   = 'slurm_acct_db'
     $slurmd_service_dropin = 'puppet:///modules/common/slurm/slurmd.service.d.dropin.conf'
 
 }
